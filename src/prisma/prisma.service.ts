7021b72d59ab02369fc21357eff018a60cda3bbd
Dineth Piyumantha <dinethpiyumantha@gmail.com>
2026-05-06 15:57:18 +0530

    feat: configure Prisma with PostgreSQL

---
+++ src/prisma/prisma.service.ts
@@ -0,0 +1,27 @@
+import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
+import { PrismaClient } from '../../generated/prisma';
+import { PrismaPg } from '@prisma/adapter-pg';
+
+@Injectable()
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  constructor() {
+    const adapter = new PrismaPg({
+      connectionString: process.env.DATABASE_URL,
+    });
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+    super({ adapter });
+  }
+
+  async onModuleInit() {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+    await this.$connect();
+  }
+
+  async onModuleDestroy() {
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-call
+    await this.$disconnect();
+  }
+}
